1b137354e8f9a4c093d518342ea41b2df66704d6
Juan Manuel <jmanuel816@gmail.com>
2016-04-01 05:41:19 -0500

    dia 13

--- Capitulo_01/01_Getting_Started:_The_Easy_Problems/03_Medium/119_:::_Greedy_Gift_Givers/Main.java
+++ Capitulo_01/01_Getting_Started:_The_Easy_Problems/03_Medium/119_:::_Greedy_Gift_Givers/Main.java
@@ -3,6 +3,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Main
 {
@@ -10,7 +13,44 @@ public class Main
 	{
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		//-----------------------------------------------------------
+		Map<String, Integer> m=new LinkedHashMap<String, Integer>();
+		int numGrupo, plata, aCuantos, seQueda, seEntrega;
+		boolean primero=true;
 		
+		for(String line, aux[]; (line=br.readLine())!=null;)
+		{
+			if(!primero) System.out.println();
+			else primero=false;
+			
+			m.clear();
+			numGrupo=Integer.parseInt(line);
+			aux=br.readLine().split(" ");
+			for(String nombre: aux) m.put(nombre, 0);
+			
+			while(numGrupo--!=0)
+			{
+				aux=br.readLine().split(" ");
+				Integer acum=m.get(aux[0]);
+				plata=Integer.parseInt(aux[1]);
+				aCuantos=Integer.parseInt(aux[2]);
+				
+				seEntrega=(aCuantos==0)?0:plata/aCuantos;
+				seQueda=plata-seEntrega*aCuantos;
+				acum+=seQueda;
+				acum-=plata;
+				m.put(aux[0], acum);
+				
+				for(int i=0; i<aCuantos; i++)
+				{
+					Integer lol=m.get(aux[i+3]);
+					lol+=seEntrega;
+					m.put(aux[i+3], lol);
+				} 
+			}
+			
+			for(Entry<String, Integer> entry : m.entrySet())
+				System.out.println(entry.getKey()+" "+entry.getValue());
+		}
 		//-----------------------------------------------------------
 		br.close();
 	}
