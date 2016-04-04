4f1f14181e26019b1f83d42130f1d9c8f86c44c3
Juan Manuel <jmanuel816@gmail.com>
2016-04-04 11:54:26 -0500

    dia 16

--- Capitulo_01/01_Getting_Started:_The_Easy_Problems/03_Medium/119_:::_Greedy_Gift_Givers/Main.c
+++ Capitulo_01/01_Getting_Started:_The_Easy_Problems/03_Medium/119_:::_Greedy_Gift_Givers/Main.c
@@ -1,43 +1,67 @@
 #include "stdio.h"
+#include "string.h"
+
+char mi [10+1][12+1];
+int mv[10+1];
+int numGrupo;
+
+void putEnMapa(char* aQuien, int acum)
+{
+	int i;	
+	for(i=0; i<numGrupo; i++)
+		if(strcmp(aQuien, mi[i])==0)
+			mv[i]=acum;
+}
+
+int getDelMapa(char* aQuien)
+{
+	int i;	
+	for(i=0; i<numGrupo; i++)
+		if(strcmp(aQuien, mi[i])==0)
+			return mv[i];
+}
 
 int main()
 {
-	int m[10+1];
-	int numGrupo, plata, aCuantos, seQueda, seEntregam i;
+
+	int plata, aCuantos, seQueda, seEntrega, i, lol, acum;
 	int primero=1;
+	char aQuien [12+1];
 	
 	while(scanf("%d", &numGrupo) != EOF)
 	{
 		if(!primero) puts("");
 		else primero=0;
 		
-		for(i=0; i<numGrupo; i++) 
-		aux=br.readLine().split(" ");
-		for(String nombre: aux) m.put(nombre, 0);
-		
-		while(numGrupo--!=0)
+		for(i=0; i<numGrupo; i++)
 		{
-			aux=br.readLine().split(" ");
-			Integer acum=m.get(aux[0]);
-			plata=Integer.parseInt(aux[1]);
-			aCuantos=Integer.parseInt(aux[2]);
+			scanf("%s", mi[i]);
+			mv[i]=0;	
+		}
+		
+		for(i=0; i<numGrupo; i++)
+		{
+			scanf("%s %d %d", aQuien, &plata, &aCuantos);
 			
+			acum=getDelMapa(aQuien);
 			seEntrega=(aCuantos==0)?0:plata/aCuantos;
 			seQueda=plata-seEntrega*aCuantos;
 			acum+=seQueda;
 			acum-=plata;
-			m.put(aux[0], acum);
+			putEnMapa(aQuien, acum);
 			
-			for(int i=0; i<aCuantos; i++)
+			while(aCuantos--)
 			{
-				Integer lol=m.get(aux[i+3]);
+				scanf("%s", aQuien);
+				lol=getDelMapa(aQuien);
 				lol+=seEntrega;
-				m.put(aux[i+3], lol);
+				putEnMapa(aQuien, lol);
 			} 
 		}
 		
-		for(Entry<String, Integer> entry : m.entrySet())
-			System.out.println(entry.getKey()+" "+entry.getValue());
+		for(i=0; i<numGrupo; i++)
+			printf("%s %d\n", mi[i], mv[i]);
 	}
+	
 	return 0;
 }
--- Capitulo_01/01_Getting_Started:_The_Easy_Problems/03_Medium/119_:::_Greedy_Gift_Givers/Main.cpp
+++ Capitulo_01/01_Getting_Started:_The_Easy_Problems/03_Medium/119_:::_Greedy_Gift_Givers/Main.cpp
@@ -1,7 +1,52 @@
 using namespace std;
 #include <cstdio>
+#include <iostream>
+#include <string>
+#include <map>
 
 int main()
 {
+	map<string, int> map;
+	int numGrupo, plata, aCuantos, seQueda, seEntrega, i, lol, acum;
+	bool primero=true;
+	string aQuien, nombres[10];
+	
+	while(scanf("%d", &numGrupo) != EOF)
+	{
+		map.clear();
+		if(!primero) puts("");
+		else primero=false;
+		
+		for(i=0; i<numGrupo; i++)
+		{
+			cin >> aQuien;
+			map[aQuien]=0;
+			nombres[i]=aQuien;
+		}
+		
+		for(i=0; i<numGrupo; i++)
+		{
+			cin >> aQuien >> plata >> aCuantos;
+			
+			acum=map[aQuien];
+			seEntrega=(aCuantos==0)?0:plata/aCuantos;
+			seQueda=plata-seEntrega*aCuantos;
+			acum+=seQueda;
+			acum-=plata;
+			map[aQuien]=acum;
+			
+			while(aCuantos--)
+			{
+				cin >> aQuien;
+				lol=map[aQuien];
+				lol+=seEntrega;
+				map[aQuien]=lol;
+			} 
+		}
+		
+		for(i=0; i<numGrupo; i++)
+			cout << nombres[i] << " " << map[nombres[i]] << endl;
+	}
+	
 	return 0;
 }
