1f0f91098a0d28586f02d61ab05a41a6ecee69d3
Juan Manuel <jmanuel816@gmail.com>
2016-04-15 00:33:34 -0500

    dia 23

--- Capitulo_01/01_Getting_Started:_The_Easy_Problems/03_Medium/10919_:::_Prerequisites?/Main.java
+++ Capitulo_01/01_Getting_Started:_The_Easy_Problems/03_Medium/10919_:::_Prerequisites?/Main.java
@@ -3,6 +3,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Main
 {
@@ -10,7 +12,36 @@ public class Main
 	{
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		//-----------------------------------------------------------
+		int k, m, c, r;
+		Set<String> cursos=new HashSet<String>();
+		boolean pasa;
 		
+		for(String line, aux[]; !(line=br.readLine()).equals("0");)
+		{
+			cursos.clear();
+			aux=line.split("\\s+");
+			k=Integer.parseInt(aux[0]);
+			m=Integer.parseInt(aux[1]);
+			
+			aux=br.readLine().split("\\s+");
+			for(String curso: aux) cursos.add(curso);
+			
+			pasa=true;
+			while(m--!=0)
+			{
+				aux=br.readLine().split("\\s+");
+				c=Integer.parseInt(aux[0]);
+				r=Integer.parseInt(aux[1]);
+				
+				for(int i=2; i<c+2 && r>0; i++)
+					if(cursos.contains(aux[i]))
+						r--;
+				
+				if(r>0) pasa=false;
+			} 
+			
+			System.out.println(pasa?"yes":"no");
+		}
 		//-----------------------------------------------------------
 		br.close();
 	}
