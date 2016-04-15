5c467d072edd836781cb789fa8d97a669d4fb635
Juan Manuel <jmanuel816@gmail.com>
2016-04-15 18:07:15 -0500

    dia 23

--- Capitulo_01/01_Getting_Started:_The_Easy_Problems/03_Medium/11586_:::_Train_Tracks/Main.java
+++ Capitulo_01/01_Getting_Started:_The_Easy_Problems/03_Medium/11586_:::_Train_Tracks/Main.java
@@ -10,7 +10,52 @@ public class Main
 	{
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		//-----------------------------------------------------------
+		int casos=Integer.parseInt(br.readLine()), males[], females[], n;
+		boolean rta;
 		
+		for(String[] aux; casos--!=0;)
+		{
+			aux=br.readLine().split("\\s+");
+			n=aux.length;
+			males=new int[n];
+			females=new int[n];
+			
+			for(int i=0; i<n; i++)
+			{
+				if(aux[i].charAt(0)=='M') males[i]++;
+				else females[i]++;
+				
+				if(aux[i].charAt(1)=='M') males[i]++;
+				else females[i]++;
+			}
+			
+			for(int i=0; i<n; i++)
+			{
+				for(int j=(i+1)%n; i!=j; j=(j+1)%n)
+				{
+					if(males[i]==0) break;
+					else
+					{
+						if(males[i]>=females[j])
+						{
+							males[i]-=females[j];
+							females[j]=0;
+						}
+						else
+						{
+							females[j]-=males[i];
+							males[i]=0;
+						}
+					}
+				}
+			}
+			
+			rta=true;
+			for(int i=0; i<n && rta; i++)
+				if(males[i]>0 || females[i]>0) rta=false;
+			
+			System.out.println(rta?"LOOP":"NO LOOP");
+		}
 		//-----------------------------------------------------------
 		br.close();
 	}
