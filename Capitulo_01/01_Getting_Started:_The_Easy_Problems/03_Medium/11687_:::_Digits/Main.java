c1c79504df88efb79c6ce02141e9d36a9f180571
Juan Manuel <jmanuel816@gmail.com>
2016-04-18 15:08:43 -0500

    dia 26

--- Capitulo_01/01_Getting_Started:_The_Easy_Problems/03_Medium/11687_:::_Digits/Main.java
+++ Capitulo_01/01_Getting_Started:_The_Easy_Problems/03_Medium/11687_:::_Digits/Main.java
@@ -10,7 +10,18 @@ public class Main
 	{
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		//-----------------------------------------------------------
-		
+		int rta;
+		for(String line, newLine; !(line=br.readLine()).equals("END");)
+		{
+			for(rta=1; true; rta++)
+			{
+				newLine=line.length()+"";
+				if(newLine.equals(line)) break;
+				line=newLine;
+			}
+			
+			System.out.println(rta);
+		}
 		//-----------------------------------------------------------
 		br.close();
 	}
