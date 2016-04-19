cb56b03758c4c0b72e40a12a1f1d407953275922
Juan Manuel <jmanuel816@gmail.com>
2016-04-19 00:29:01 -0500

    dia 27

--- Capitulo_01/01_Getting_Started:_The_Easy_Problems/03_Medium/12478_:::_Hardest_Problem_Ever_(Easy)/Main.java
+++ Capitulo_01/01_Getting_Started:_The_Easy_Problems/03_Medium/12478_:::_Hardest_Problem_Ever_(Easy)/Main.java
@@ -3,14 +3,77 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
 
 public class Main
 {
 	public static void main(String[] args) throws IOException
 	{
+		//solucion();
+		System.out.println("KABIR");		
+	}
+	
+	private static void solucion() throws IOException
+	{
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		//-----------------------------------------------------------
+		int n=Integer.parseInt(br.readLine());
+		char[] aux;
 		
+		Map<String, Integer> nombres=new HashMap<String, Integer>();
+		while(n--!=0)
+		{
+			aux=br.readLine().toCharArray();
+			Arrays.sort(aux);
+			nombres.put(new String(aux), 0);
+		}
+		
+		char[][] sopa=new char[9][9];
+		for(int i=0; i<9; i++)
+		{
+			aux=br.readLine().toCharArray();
+			for(int j=0; j<9; j++) sopa[i][j]=aux[j];
+		}
+		
+		String nombre, ordenado;
+		Integer posible;
+		for(int i=0; i<9; i++)
+		{
+			for(int j=0; j<9; j++)
+			{
+				nombre="";
+				for(int k=j; k<9; k++)
+				{
+					nombre+=sopa[i][k];
+					aux=nombre.toCharArray();
+					Arrays.sort(aux);
+					ordenado=new String(aux);
+					posible=nombres.get(ordenado);
+					if(posible!=null) nombres.put(ordenado, posible+1);
+				}
+			}
+		}
+		
+		for(int j=0; j<9; j++)
+		{
+			for(int i=0; i<9; i++)
+			{
+				nombre="";
+				for(int k=i; k<9; k++)
+				{
+					nombre+=sopa[k][j];
+					aux=nombre.toCharArray();
+					Arrays.sort(aux);
+					ordenado=new String(aux);
+					posible=nombres.get(ordenado);
+					if(posible!=null) nombres.put(ordenado, posible+1);
+				}
+			}
+		}
+		
+		System.out.println(nombres);
 		//-----------------------------------------------------------
 		br.close();
 	}
--- Capitulo_02/01_Linear_Data_Structures_with_Built-in_Libraries/01_1D_Array_Manipulation/10038_:::_Jolly_Jumpers_★/Main.java
+++ Capitulo_02/01_Linear_Data_Structures_with_Built-in_Libraries/01_1D_Array_Manipulation/10038_:::_Jolly_Jumpers_★/Main.java
@@ -10,7 +10,36 @@ public class Main
 	{
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		//-----------------------------------------------------------
-		
+		int nums[], n, necesarios[], dif;
+		for(String linea, aux[]; (linea=br.readLine())!=null;)
+		{
+			aux=linea.split(" ");
+			n=Integer.parseInt(aux[0]);
+			nums=new int[n];
+			necesarios=new int[n];
+			
+			for(int i=0; i<n; i++)
+			{
+				nums[i]=Integer.parseInt(aux[i+1]);
+				if(i>0)
+				{
+					dif=Math.abs(nums[i]-nums[i-1]);
+					if(dif<n) necesarios[dif]++;
+				}
+			}
+			
+			boolean jolly=true;
+			for(int i=1; i<n; i++)
+			{
+				if(necesarios[i]==0)
+				{
+					jolly=false;
+					break;
+				}
+			}
+
+			System.out.println(jolly?"Jolly":"Not jolly");
+		}
 		//-----------------------------------------------------------
 		br.close();
 	}
