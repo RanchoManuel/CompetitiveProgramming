70e9226e5eb7458c00ed3d61ed1bcfa0fde26691
Juan Manuel <jmanuel816@gmail.com>
2016-04-20 23:48:25 -0500

    dia 28

--- Capitulo_01/01_Getting_Started:_The_Easy_Problems/03_Medium/11956_:::_Brainfuck/Main.java
+++ Capitulo_01/01_Getting_Started:_The_Easy_Problems/03_Medium/11956_:::_Brainfuck/Main.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main
 {
@@ -10,7 +11,34 @@ public class Main
 	{
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		//-----------------------------------------------------------
+		int casos=Integer.parseInt(br.readLine()), pointer, mem[]=new int[100];
+		char inst[];
+		String aux;
 		
+		for(int c=1; c<=casos; c++)
+		{
+			Arrays.fill(mem, 0);
+			pointer=0;
+			inst=br.readLine().toCharArray();
+			for(char ch: inst)
+			{
+				if(ch=='>') 		pointer=(pointer+1)%100;
+				else if(ch=='<') pointer=(pointer+100-1)%100;
+				else if(ch=='+') mem[pointer]=(mem[pointer]+1)%256;
+				else if(ch=='-') mem[pointer]=(mem[pointer]+256-1)%256;
+				else if(ch=='.');
+			}
+
+			System.out.printf("Case %d:", c);
+			for(int i=0; i<mem.length; i++)
+			{
+				System.out.print(" ");
+				aux=Integer.toHexString(mem[i]).toUpperCase();
+				aux=aux.length()>1?aux:"0"+aux;
+				System.out.print(aux);
+			}
+			System.out.println();
+		}
 		//-----------------------------------------------------------
 		br.close();
 	}
