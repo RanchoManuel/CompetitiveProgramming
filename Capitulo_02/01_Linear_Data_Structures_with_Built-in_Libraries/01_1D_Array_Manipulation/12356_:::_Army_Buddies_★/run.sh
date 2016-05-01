c25704a9d7e459bfb3d98e51c49a5c57f393387e
Juan Manuel <jmanuel816@gmail.com>
2016-04-30 23:33:03 -0500

    dia 35

--- Capitulo_02/01_Linear_Data_Structures_with_Built-in_Libraries/01_1D_Array_Manipulation/12356_:::_Army_Buddies_★/run.sh
+++ Capitulo_02/01_Linear_Data_Structures_with_Built-in_Libraries/01_1D_Array_Manipulation/12356_:::_Army_Buddies_★/run.sh
@@ -1,12 +1,12 @@
 clear
-#javac Main.java
-#java Main < in.txt
-#echo "-------\nRta: $?\n-------"
+javac Main.java
+java Main < in.txt
+echo "-------\nRta: $?\n-------"
 
 #g++ -o MainCpp Main.cpp
 #./MainCpp < in.txt
 #echo "-------\nRta: $?\n-------"
 
-#gcc -o MainC Main.c
-#./MainC < in.txt
-#echo "-------\nRta: $?\n-------"
+gcc -o MainC Main.c
+./MainC < in.txt
+echo "-------\nRta: $?\n-------"
